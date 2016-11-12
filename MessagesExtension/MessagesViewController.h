ffe6c4d9bb72cef5c535b8f8532de6e2a8135918
tyrocca <tyler.rocca@gmail.com>
2016-11-12 15:37:00 -0500

    for will

---
+++ MessagesExtension/MessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.h
+//  MessagesExtension
+//
+//  Created by Ty Rocca on 11/12/16.
+//  Copyright © 2016 Ty Rocca. All rights reserved.
+//
+
+#import <Messages/Messages.h>
+
+@interface MessagesViewController : MSMessagesAppViewController
+
+@end
---
+++ index.ios.js
@@ -0,0 +1,46 @@
+
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    ScrollView,
+    NativeModules,
+} from 'react-native';
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
+
+class AmazingMessageExtension extends Component {
+
+    render() {
+        console.log("FOO BAR BAZ");
+        return (
+            <ScrollView style={styles.container}>
+            <Text style={styles.welcome}>
+            Welcome to React Native!
+            </Text>
+            <Text style={styles.instructions}>
+            To get started, edit index.ios.js
+            </Text>
+            <Text style={styles.instructions} onPress={() => NativeModules.DevMenu.reload()}>
+            Press Me to Reload!
+            </Text>
+            </ScrollView>
+        );
+    }
+}
+AppRegistry.registerComponent('AmazingMessageExtension', () => AmazingMessageExtension);
